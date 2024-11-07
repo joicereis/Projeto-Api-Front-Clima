51b92da8432ccc5b5879a372c3117b6114fc10c8
joice.jdosreis@gmail.com <joice.jdosreis@gmail.com>
2024-11-07 20:00:13 -0300

    Primeira versão do projeto Clima

---
+++ src/README.txt
@@ -0,0 +1,11 @@
+Nesse projeto foi construído a aplicação Web 'Clima' que permite visualizar condições do clima de acordo com a cidade consultada.
+Os serviços utilizados para consulta dos dados do clima são fornecidos pela api pública 'Weather API' que pode ser acessa através do link https://www.weatherapi.com/weather/
+
+IMPORTANTE!
+Passos utilização dessa API é necessário gerar uma chave de autenticação. Segue abaixo passos para autenticação:
+1- Se cadastre no site através do link https://www.weatherapi.com/signup.aspx
+2- Realize a confirmação de cadastro através de link enviado por email e realize login pelo site.
+3- Ao ser direcionado para o seu perfil (https://www.weatherapi.com/my/), é possível visualizar o campo API Key que contém o valor da chave a ser utilizada na autenticação, copie o valor desse campo.
+4- Acesse o arquivo 'index.js' disponibilizado no caminho 'src\js' desse projeto e na linha 7, insira entre as aspas o valor copiado. Por fim, salve as alterações realizadas.
+5- Realize a abertura do arquivo 'index.html', disponível no diretório 'src', para visualizar e utilizar a aplicação.
+
---
+++ src/js/index.js
@@ -0,0 +1,44 @@
+/* Passos:
+1- Digitar nome da cidade
+2- Clicar no botão de busca
+3- Enviar dados para a API
+4- Representar dados retornados pela API em tela.
+*/
+const chaveApi ="" //Insira aqui a chave/token gerada pela API
+const botaoBusca = document.querySelector(".btn-busca");
+
+botaoBusca.addEventListener("click", async() => {
+    const cidade = document.getElementById("input-busca-cidade").value;
+    //if(!cidade) return; //se não houver nada na cidade, interrompe a busca
+
+    const dados = await buscarDadosDeClima(cidade);
+    preencherDadosNaTela(dados, dados.location.name);
+})
+
+async function buscarDadosDeClima(cidade){
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${chaveApi}&q=${cidade}&aqi=no&lang=pt`
+    const resposta = await fetch(apiUrl); // o fetch retorna o objeto de acordo com a url passada.
+    
+    if(resposta.status !== 200) return;
+    
+    const dados= await resposta.json();
+    console.log(dados);
+    return dados;
+}
+
+function preencherDadosNaTela(dados, cidadeEncontrada){
+    const estado = dados.location.region;
+    const temperatura = Math.round(dados.current.temp_c);
+    const umidade = dados.current.humidity;
+    const condicao = dados.current.condition.text;
+    const velocidadeVento = dados.current.wind_kph;
+    const iconeCondicao = "https:" + dados.current.condition.icon;
+
+    document.getElementById("cidade").textContent = cidadeEncontrada;
+    document.getElementById("estado").textContent = estado;
+    document.getElementById("temperatura").textContent = `${temperatura}°C`;
+    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao );
+    document.getElementById("condicao").textContent = condicao;
+    document.getElementById("umidade").textContent = umidade;
+    document.getElementById("velocidade-vento").textContent = velocidadeVento;
+}
