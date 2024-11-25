30788efd39daf4fccd61c59fc728ae5cdc769e1a
joice.jdosreis@gmail.com <joice.jdosreis@gmail.com>
2024-11-25 17:08:18 -0300

    Ajuste de nomes de classes

--- src/js/index.js
+++ src/js/index.js
@@ -1,44 +1,48 @@
 /* Passos:
-1- Digitar nome da cidade
-2- Clicar no botão de busca
+1- Digitar nome da cidade/ city
+2- Clicar no botão de busca/ search
 3- Enviar dados para a API
 4- Representar dados retornados pela API em tela.
 */
-const chaveApi = "" //Insira entre aspas a chave/token gerada pela API.
-const botaoBusca = document.querySelector(".btn-busca");
 
-botaoBusca.addEventListener("click", async() => {
-    const cidade = document.getElementById("input-busca-cidade").value;
+const btnSearch = document.querySelector(".btn-search");
+
+btnSearch.addEventListener("click", async() => {
+    const city = document.getElementById("input-search-city").value;
+
     //if(!cidade) return; //se não houver nada na cidade, interrompe a busca
-
-    const dados = await buscarDadosDeClima(cidade);
-    preencherDadosNaTela(dados, dados.location.name);
+    const dados = await getDadosDeClima(city);
+    printDadosNaTela(dados, dados.city);
 })
 
-async function buscarDadosDeClima(cidade){
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${chaveApi}&q=${cidade}&aqi=no&lang=pt`
-    const resposta = await fetch(apiUrl); // o fetch retorna o objeto de acordo com a url passada.
-    
-    if(resposta.status !== 200) return;
-    
-    const dados= await resposta.json();
-    console.log(dados);
-    return dados;
+async function getDadosDeClima(city){
+        const apiUrl = `http://localhost:5146/api/clima/${city}`; 
+    try{
+        const resposta = await fetch(apiUrl); // o fetch retorna o objeto de acordo com a url passada.
+
+        if(resposta.status != 200){
+            throw new Error("Erro ao buscar a cidade.");
+        } 
+        const dados= await resposta.json();
+        console.log(dados); //teste para verificar se os dados estão sendo recebidos
+        return dados;
+
+    }catch(error){
+        console.error("Erro: ", error);
+    }
 }
 
-function preencherDadosNaTela(dados, cidadeEncontrada){
-    const estado = dados.location.region;
-    const temperatura = Math.round(dados.current.temp_c);
-    const umidade = dados.current.humidity;
-    const condicao = dados.current.condition.text;
-    const velocidadeVento = dados.current.wind_kph;
-    const iconeCondicao = "https:" + dados.current.condition.icon;
+function printDadosNaTela(dados, city){
+  
+    //const tempC = Math.round(dados.tempCelsius);
+    const iconCondition = "https:" + dados.iconCondition;
 
-    document.getElementById("cidade").textContent = cidadeEncontrada;
-    document.getElementById("estado").textContent = estado;
-    document.getElementById("temperatura").textContent = `${temperatura}°C`;
-    document.getElementById("icone-condicao").setAttribute("src", iconeCondicao );
-    document.getElementById("condicao").textContent = condicao;
-    document.getElementById("umidade").textContent = umidade;
-    document.getElementById("velocidade-vento").textContent = velocidadeVento;
+    document.getElementById("city").textContent = dados.city;
+    document.getElementById("region").textContent = dados.region;
+    document.getElementById("temp-c").textContent = Math.round(dados.tempCelsius) + "°C";
+    document.getElementById("icon-condition").setAttribute("src", iconCondition );
+    document.getElementById("condition").textContent = dados.condition;
+    document.getElementById("humidity").textContent = dados.humidity;
+    document.getElementById("wind-speed").textContent = dados.windSpeed;
+    
 }
